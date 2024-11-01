76e3a16be5195f347fbbfd10d129b24412031a0e
erentaskiran <101654590+erentaskiran@users.noreply.github.com>
2024-11-01 12:24:39 +0300

    project structure init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module org.example.hastane {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.hastane to javafx.fxml;
+    exports org.example.hastane;
+    exports;
+    opens to
+}
